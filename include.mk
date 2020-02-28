50d58cdd6b4484e2e3a4d3a3844155436ecc0bfd
Alexander Ben Nasrallah <me@abn.sh>
2020-02-28 08:44:01 +0100

    add CABAL_ARGS to Makefile

--- include.mk
+++ include.mk
@@ -3,15 +3,15 @@ all:
 	$(MAKE) test
 
 build:
-	cabal v2-build -f pedantic $(PACKAGE)
+	cabal v2-build -f pedantic $(CABAL_ARGS) $(PACKAGE)
 
 test: $(TESTS-SUITES)
 
 $(TESTS-SUITES):
-	cabal v2-run -f pedantic $(PACKAGE):$@ -- $(TEST_ARGS)
+	cabal v2-run -f pedantic $(CABAL_ARGS) $(PACKAGE):$@ -- $(TEST_ARGS)
 
 check:
-	cabal check
+	cabal $(CABAL_ARGS) check
 	cabal-fmt  $(PACKAGE).cabal | diff $(PACKAGE).cabal -
 	hfmt
 
